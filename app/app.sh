c0014449da9bc1df8b76a82c9212b8e23605901d
Kristina Vodorezova <164237440+vodor001@users.noreply.github.com>
2024-08-14 16:31:11 +0200

    Update app.sh

--- app/app.sh
+++ app/app.sh
@@ -10,9 +10,9 @@
 # Clone the repository of your tool and checkout to one specific commit. 
 #########################################################
 
-# git clone https://github.com/odtp-org/tool-example.git /odtp/odtp-workdir/tool-example
-# cd /odtp/odtp-workdir/tool-example
-# git checkout xxxxxxxxxxxx
+git clone https://github.com/vodor001/test_split.py.git /odtp/odtp-workdir/test_split
+cd /odtp/odtp-workdir/test_split
+git checkout v0.1.0
 
 #########################################################
 # 2. CONFIG FILE CONFIGURATION
@@ -25,7 +25,7 @@
 # 3. INPUT FOLDER MANAGEMENT
 #########################################################
 
-# ln -s /odtp/odtp-input/... /odtp/odtp-workdir/...
+ln -s /odtp/odtp-input/test_split/data /odtp/odtp-workdir/test_split/data
 
 #########################################################
 # 4. TOOL EXECUTION
@@ -33,13 +33,14 @@
 #########################################################
 
 # COMMAND $PARAMETER_A #PARAMETER_B /odtp/odtp-input/data
+python split.py /../../odtp-input/test_split/data
 
 #########################################################
 # 5. OUTPUT FOLDER MANAGEMENT
 # The selected output files generated should be placed in the output folder
 #########################################################
 
-# cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output
+cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output/test_split/data
 
 ############################################################################################
 # END OF MANUAL USER APP
